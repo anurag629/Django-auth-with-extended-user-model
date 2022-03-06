d06c91b421aa53d211ab513945586e16fc34d746
ANURAG VERMA <20cse08@ietagra.org.in>
2022-03-06 22:26:31 +0530

    created managers

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    pic = models.ImageField(upload_to='profile')
+    address_line1 = models.CharField(max_length=50)
+    city = models.CharField(max_length=20)
+    state = models.CharField(max_length=20)
+    pincode = models.IntegerField()
+
+    USERNAME_FIELD = 'email'
